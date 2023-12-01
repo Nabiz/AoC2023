a95ca7f9e52bd09632bdda60d70f0aa3d49bad37
Nabiz <dawcio.gwizdz@gmail.com>
2023-12-01 13:51:00 +0100

    Day01 part 2

--- 01/main.cpp
+++ 01/main.cpp
@@ -5,6 +5,30 @@
 
 using namespace std;
 
+int preplace(string& input, const string& a, const string& b){
+    size_t index;
+    int n = 0;
+    while((index = input.find(a,0)) != string::npos){
+        input.replace(index, a.size(), b);
+        n++;
+    }
+    return n;
+}
+
+void replaceSpelledDigits(string& s)
+{
+    preplace(s, "one", "o1ne");
+    preplace(s, "two", "t2wo");
+    preplace(s, "three", "t3hree");
+    preplace(s, "four", "f4our");
+    preplace(s, "five", "f5ive");
+    preplace(s, "six", "s6ix");
+    preplace(s, "seven", "s7even");
+    preplace(s, "eight", "e8ight");
+    preplace(s, "nine", "n9ine");
+    preplace(s, "zero", "z0ero");
+}
+
 int main()
 {
     fstream inputFile;
@@ -24,7 +48,37 @@ int main()
                     digitsInLine.push_back(character-48);
                 }
             }
-            sum += 10*digitsInLine[0] + digitsInLine[digitsInLine.size()-1];
+            if(!digitsInLine.empty())
+            {
+                sum += 10*digitsInLine[0] + digitsInLine[digitsInLine.size()-1];
+            }
+        }
+    }
+    inputFile.close();
+    cout << sum << endl;
+    
+    // //PART 2
+    sum = 0;
+    inputFile.open("input", ios::in);
+    if(inputFile.is_open())
+    {
+        string line;
+        while (getline(inputFile, line))
+        {   
+            replaceSpelledDigits(line);
+            vector<int> digitsInLine;
+            for(char character: line)
+            {
+                if(isdigit(character))
+                {
+                    digitsInLine.push_back(character-48);
+                }
+            }
+            if(!digitsInLine.empty())
+            {
+                cout << 10*digitsInLine[0] + digitsInLine[digitsInLine.size()-1] << endl;
+                sum += 10*digitsInLine[0] + digitsInLine[digitsInLine.size()-1];
+            }
         }
     }
     inputFile.close();
